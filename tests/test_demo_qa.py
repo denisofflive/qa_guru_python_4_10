b915214f1da7e6686f6292fe84150a65fdfa4c68
Denis Denisov <denisofflive@mail.ru>
2023-05-29 16:56:49 +0300

    add edit

--- tests/test_demo_qa.py
+++ tests/test_demo_qa.py
@@ -1,6 +1,5 @@
 from demo_qa import registration_form
 
-
 def test_fill_fields(browser_setup):
     registration_page = registration_form.RegistrationPage()
 
@@ -8,10 +7,10 @@ def test_fill_fields(browser_setup):
     registration_page.open()
     registration_page.fill_first_name('Denis')
     registration_page.fill_last_name('Denisov')
-    registration_page.fill_email('denisofflive@mail.ru')
+    registration_page.fill_email('denisqaguru@gmail.com')
     registration_page.fill_date_of_birth('15', 'March', '1985')
     registration_page.gender('Male')
-    registration_page.type_phone('79776136665')
+    registration_page.type_phone('9776136677')
     registration_page.select_subjects('Maths')
     registration_page.select_hobbies('Music')
     registration_page.upload_picture()
@@ -24,10 +23,10 @@ def test_fill_fields(browser_setup):
     # THEN
     registration_page.assert_registred_user_info(
         'Denis Denisov',
-        'denisofflive@mail.ru',
+        'denisqaguru@gmail.com',
         'Male',
-        '79776136665',
-        '15 March,1985',
+        '9776136677',
+        '15 March,1994',
         'Maths',
         'Music',
         'picture.jpg',
